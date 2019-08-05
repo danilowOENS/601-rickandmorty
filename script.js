0954c5199aea7dddfca4a0b3399052941913bbfd
danilo <dl539994@gmail.com>
2019-08-05 16:23:37 -0300

    Adicionar-logica do jogo

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let caixaDetexto = document.querySelector("input");
 let iniciar = document.querySelector("#iniciar");
 let tentar = document.querySelector("#tentar");
-let div = document.querySelector("div");
+let h4= document.querySelector("h4");
 let imagem = document.querySelector("img");
+let p = document.querySelector("p");
+let contador = 0;
 
 gerarImagem = () =>{
     let aleatorio = Math.ceil(Math.random()*493);
@@ -13,7 +15,26 @@ gerarImagem = () =>{
     .then(function(data){
         console.log(data.name);
         imagem.src = data.image;
+        let nomeDoPersonagem = data.name.toUpperCase().toString();
+
+        tentar.onclick =function(){
+            let input = caixaDetexto.value.toUpperCase().toString();
+            if(input == nomeDoPersonagem){
+                h4.innerHTML = `certou`
+                contador ++;
+                p.innerHTML = `pontuacão: ${contador}`;
+                caixaDetexto.value = "";
+            }
+            else{
+                h4.innerHTML = `errrouuuuuu`
+                contador --;
+                p.innerHTML = `pontuação: ${contador}`;
+                caixaDetexto.value = "";
+            }
+            gerarImagem();
+        }    
     })
+    
 } 
 
 iniciar.onclick = gerarImagem;
